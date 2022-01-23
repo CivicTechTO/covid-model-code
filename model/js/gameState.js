15046f5895d6da163dd51ada795c8db3fd1320bd
John Spragge <john.spragge@manifoldeye.ca>
2022-01-23 18:31:29 -0500

    fixed document identifier reference

--- model/js/gameState.js
+++ model/js/gameState.js
@@ -32,9 +32,9 @@ class GameState extends TownState
 
 		if (document.getElementById (C.SLIDER))
   			this.chartList = new SliderCharts (this, graphedValues, C.SLIDER);
-		else if (document.getElementById (C.CHART_DESCRIPTIONS.MOVING))
+		else if (document.getElementById (C.CHART_DESCRIPTIONS.MOVING.id))
 			this.chartList = new TwinCharts (this, graphedValues);
-		else this.chartList = new SingleChart (his, graphedValues);
+		else this.chartList = new SingleChart (this, graphedValues);
 
 		this.run = true;
 		this.past = null;
